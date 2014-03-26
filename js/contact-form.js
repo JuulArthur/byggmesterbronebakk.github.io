0026b197e9210dfd2ff56d4e7eece4a30e487b51
Juul Arthur Ribe Rudihagen <juularthur92@gmail.com>
2014-03-26 20:45:27 +0100

    testing send mail

--- js/contact-form.js
+++ js/contact-form.js
@@ -1,9 +1,9 @@
 
 jQuery(document).ready(function(){
-	
+
 	//if submit button is clicked
-	$('#submit').click(function () {		
-		
+	$('#submit').click(function () {
+
 		//Get the data from all the fields
 		var name = $('input[name=name]');
 		var email = $('input[name=email]');
@@ -16,65 +16,63 @@ jQuery(document).ready(function(){
 			name.addClass('hightlight');
 			return false;
 		} else name.removeClass('hightlight');
-		
+
 		if (email.val()=='') {
 			email.addClass('hightlight');
 			return false;
 		} else email.removeClass('hightlight');
 
 		//E-mail address validation
-		
+
 		var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-		if(reg.test(email.val()) == false) {		 
+		if(reg.test(email.val()) == false) {
 			email.addClass('hightlight');
 			return false;
-		} else email.removeClass('hightlight');	  
-		
+		} else email.removeClass('hightlight');
+
 		if (comment.val()=='') {
 			comment.addClass('hightlight');
 			return false;
 		} else comment.removeClass('hightlight');
-		
+
 		//organize the data properly
-		var data = 'name=' + name.val() + '&email=' + email.val() + '&subject=' + 
+		var data = 'name=' + name.val() + '&email=' + email.val() + '&subject=' +
 		subject.val() + '&comment='  + encodeURIComponent(comment.val());
-		
+
 		//disabled all the text fields
 		$('.contact input, .contact textarea').attr('disabled','true');
-		
+
 		//show the loading sign
 		$('.loading').show();
-		
+
 		//start the ajax
 		$.ajax({
 			//this is the php file that processes the data and send mail
-			url: "contact-form/send.php",	
-			
-			//GET method is used
-			type: "GET",
+			url: "https://mandrillapp.com/api/1.0/messages/send.json",
 
-			//pass the data			
-			data: data,		
-			
-			//Do not cache the page
-			cache: false,
-			
-			//success
-			success: function (html) {				
-				//if process.php returned 1/true (send mail success)
-				if (html==1) {					
-					//hide the form
-					$('.contact-form').hide(600);					
-					
-					//show the success message
-					$('.form-success').fadeIn('slow');
-					
-				//if process.php returned 0/false (send mail failed)
-				} else alert('An unexpected error occured. Please try again later.');				
-			}		
-		});
-		
-		//cancel the submit button default behaviours
-		return false;
-	});	
-});	
+			//GET method is used
+			type: "POST",
+
+			data: {
+		    ‘key’: ‘KXaPKsnltOmpYNymwVEmIA’,
+		    ‘message’: {
+		      ‘from_email’: email,
+		      ‘to’: [
+		          {
+		            ‘email’: ‘juularthur92@gmail.com’,
+		            ‘name’: name,
+		            ‘type’: ‘to’
+		          },
+		        ],
+		      ‘autotext’: ‘true’,
+		      ‘subject’: subject,
+		      ‘html’: comment
+		    }
+		  }
+		}).done(function(response) {
+   	$('.contact-form').hide(600);
+
+		//show the success message
+		$('.form-success').fadeIn('slow');
+ 	});
+});
